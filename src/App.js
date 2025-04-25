d4becc096fb8ca365f72636ad6adeaf163f41973
Sandro Pereira <sandrodcpereira@gmail.com>
2025-04-25 15:12:28 +0100

    Sheet done

--- src/App.js
+++ src/App.js
@@ -2,38 +2,19 @@
 import React, { useEffect, useState } from 'react';
 import ConfettiBoom from 'react-confetti-boom';
 import { createBottomSheet } from '@plainsheet/core';
-import './App.css';
+// import './App.css';
 import './style.css';
+
+import './styles/main.scss';
+
 
 
 function App() {
   // sheet
 
-  useEffect(() => {
-    const bottomSheet = createBottomSheet({
-      content: '<h2>Hello</h2><p>This is a custom sheet!</p>',
-      options: {
-        position: 'bottom',
-        duration: 300,
-      },
-    });
-
-    bottomSheet.mount();
-
-    return () => {
-      bottomSheet.unmount();
-    };
-  }, []);
-
   const showSheet = () => {
     const bottomSheet = createBottomSheet({
-      content: `
-        <div className="main-container overlay">
-          <div className="controls">
-              <p>About</p>
-              <button id="close-btn">Close</button>
-          </div>
-
+  content: `  
           <div className="content">
               <img src="/assets/sparkle.png" alt="" style="width: 54px" />
               <p><span>Joyfulist</span> was created by <a href="https://www.theonboardinglab.com/" target="_blank">Clare</a> and <a href="https://sandro.design" target="_blank">Sandro</a> in hopes to bring a little more joy into your life.</p>
@@ -44,17 +25,21 @@ function App() {
 
               <p>Add to your home screen by tapping <span>Share > Add to Home Screen</span> on your phone.</p>
           </div>
-        </div>
+          <!--div><button id="close-btn">Close</button></div-->
     `,
-      options: {
-        position: 'bottom',
-        duration: 300,
-      },
-    });
+    draggingAnimationTimings: "spring",
+    expandable: false,
+    dragTriggers: [".pbs-content"],
+    backdropColor: "rgba(0, 0, 0, 0.20)",
+    draggingAnimationDuration: 500,
+
+});
 
     bottomSheet.mount();
     bottomSheet.open();
   };
+
+
 
   // confet
   const [showConfetti, setShowConfetti] = useState(false);
@@ -93,23 +78,25 @@ function App() {
       <header>
           <button id="about" onClick={showSheet}>About</button>
 
-          <button onClick={handleClick}>Celebrate!</button>
+          <button onClick={handleClick}>Confetti</button>
       </header>
 
-      <div class="logo-container">
+      <div className="logo-container">
           <h1>Joyfulist</h1>
           <h2>Your daily dose of joy</h2>
       </div>
 
-      <div class="main-container">
-          <div class="controls">
+      <div className="content-container">
+          <div className="controls">
               <p>Your tasks for today</p>
               <button id="refresh-btn">Refresh</button>
           </div>
 
-          <div id="tasks-container"></div>
+          <div id="tasks">
+            [tasks go here]
+          </div>
 
-          <div class="share">
+          <div className="share">
               <button id="share">Share with your friends</button>
           </div>
       </div>
